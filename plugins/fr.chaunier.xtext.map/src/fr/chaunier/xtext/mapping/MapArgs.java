b83f2e9a08cfc0b0b16c28ff20d0df15d0583aaf
p.chaunier <p.Chaunier@gmail.com>
2011-11-13 18:24:33 +0000

    new code

---
+++ plugins/fr.chaunier.xtext.map/src/fr/chaunier/xtext/mapping/MapArgs.java
@@ -0,0 +1,14 @@
+package fr.chaunier.xtext.mapping;
+
+import java.util.List;
+
+import fr.chaunier.xtext.mapping.mapDsl.InOut;
+import fr.chaunier.xtext.mapping.mapDsl.RuleDef;
+
+public class MapArgs {
+	public String name ;
+	public List<String> args ;
+	public String description ;
+}
+
+
